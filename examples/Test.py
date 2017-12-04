0cfdac797b2ebdf1e3725da2d95cdfcb2ffc9227
sebastian <sebi-otten@gmx.de>
2017-12-04 09:01:10 +0100

    added new testscript and changed Test.py class

--- examples/Test.py
+++ examples/Test.py
@@ -7,54 +7,26 @@ from TimingInfo import TimingInfoAll
 
 
 
-class Test:
+# class represents one run of an example and contains information such as input
+# parameters, the raw text-output of the examples and some information that is
+# required to build the example and run it (e.g. the folder-name etc.)
+class ExampleRun:
     def __init__(self, homedir):
-        self._optionslist = []
-        self._backend = "backend_cpu"
-        self._numthreads = 1
+        self._args = []
         self._hasfinished = False
         self._homedir = homedir # root dir of test, eg. Opt/examples/image_warping
         self._output = "Test hasn't been run yet!"
-        self._finalCost = None
-        self._timeInfoAll = None
-        self._referenceCost = None
-        self._tolerance = None # relative tolerance
-        self._checkResult = False
+        self._execCommand = ""
         self._printOutput = False # if true, self._output is printed after test completion
 
-    def setBackend(self, backend):
-        self._backend = backend
-
-    def getBackend(self):
-        return self._backend
-
-    def setNumthreads(self, numthreads):
-        self._numthreads = int(numthreads)
-
-    def getNumthreads(self):
-        return self._numthreads
-
-    def setReferenceCost(self, cost):
-        self._referenceCost = cost
-
-    def setTolerance(self, tol):
-        self._tolerance = tol
-
-    def getKernelNames(self):
-        return self._timeInfoAll
-
-    def getTimeForKernel(self, kernelName):
-        return self._timeInfoAll.getTimeForKernel(kernelName)
 
     def run(self):
         os.chdir(self._homedir)
 
         sp.Popen("make").communicate()
 
-        callcommand = ["./"+self._homedir,
-                "--backend", self._backend,
-                "--numthreads", str(self._numthreads),
-                ]
+        callcommand = [self._execCommand] + self._args
+        print(callcommand)
         process = sp.Popen(callcommand, stdout=sp.PIPE)
         self._output = process.communicate()[0]
         if self._printOutput == True:
@@ -63,17 +35,18 @@ class Test:
         os.chdir("..")
 
         self._hasfinished = True
-        self.parseOutput()
 
-        if self._checkResult:
-            relerror = abs(float(self._referenceCost) - self._finalCost)/self._finalCost
-            print'asdf'
-            print relerror
-            print'asdf'
-            if relerror > self._tolerance:
-                print "ERROR={0} IN FINAL COST IS LARGER THAN TOL={1}".format(relerror, self._tolerance)
-            else:
-                print "final cost is OK"
+        # self.parseOutput()
+
+        # if self._checkResult:
+        #     relerror = abs(float(self._referenceCost) - self._finalCost)/self._finalCost
+        #     print'asdf'
+        #     print relerror
+        #     print'asdf'
+        #     if relerror > self._tolerance:
+        #         print "ERROR={0} IN FINAL COST IS LARGER THAN TOL={1}".format(relerror, self._tolerance)
+        #     else:
+        #         print "final cost is OK"
 
     def printOutput(self):
         print self._output
@@ -81,112 +54,38 @@ class Test:
     def getOutput(self):
         return self._output
 
-    def printInfo(self):
-        print("\n\n")
-        self.printInfoStart()
-        self.printInfoEnd()
-        print("\n\n")
+    def getCallCommand(self):
+        return [self._execCommand] + self._args
+
 
     def printInfoStart(self):
         print("\n\n")
         print("START Test: " + self._homedir)
-        print "backend: " + self._backend
-        print "numthreads: " + str(self._numthreads)
+        print("commando was TODO")
 
 
     def printInfoEnd(self):
         print "simulation finished by testrunner"
-        print "final cost was " + str(self._finalCost)
-        print self._timeInfoAll
         print("END Test")
         print("\n\n")
 
    
     def parseOutput(self):
-        self.parseFinalCost()
-        ti = TimingInfoAll()
-        ti.parseOutput(self._output)
-        self._timeInfoAll = ti
+        pass
+        # self.parseFinalCost()
+        # ti = TimingInfoAll()
+        # ti.parseOutput(self._output)
+        # self._timeInfoAll = ti
 
     def parseFinalCost(self):
-        match = re.search("final cost=(.*)", self._output)
-        self._finalCost = float(match.group(1))
+        pass
+        # match = re.search("final cost=(.*)", self._output)
+        # self._finalCost = float(match.group(1))
 
 
     def parseTime(self, kernelName):
-        match = makeKernelRegex(kernelName).search(self._output)
-        self._timingValue[kernelName] = float(match.group(2))
-        self._timingUnit[kernelName] = match.group(3)
-
-
-class TestGroup:
-    def __init__(self):
-        self._tests = []
-
-    def addTest(self, test):
-        self._tests.append(test)
-
-    def printScalingInfo(self, relorabs):
-        testlist = self.getTestsWithBackend('backend_cpu_mt')
-        numthreadslist = [t.getNumthreads() for t in testlist]
-        numthreadslist = list(set(numthreadslist)) # eliminate duplicates and sort
-        numthreadslist.sort()
-        numthreadslist = [str(n) for n in numthreadslist] # convert to string values
-
-        numthreadsline = "{:50}".format("numthreads") + "{:^10}".format("cuda") + "{:^10}".format("cpu") + "".join(["{:^10}".format(str(n)) for n in numthreadslist])
-        print numthreadsline
-
-        # get kernelnames
-        # pdb.set_trace()
-        kernelNames = testlist[0]._timeInfoAll.getKernelNames()
-        for name in kernelNames:
-            line = ""
-            absline = line + ("{:50}".format(name + "(abs)"))
-            relline = line + ("{:50}".format(name + "(rel)"))
-            abstimes = []
-
-            t = self.getTestWithBackendAndNumthreads('backend_cpu', 1)
-            abstimes.append(t.getTimeForKernel(name))
-
-            t = self.getTestWithBackendAndNumthreads('backend_cuda', 1)
-            abstimes.append(t.getTimeForKernel(name))
-
-            for num in numthreadslist:
-                t = self.getTestWithBackendAndNumthreads('backend_cpu_mt', num)
-                abstimes.append(t.getTimeForKernel(name))
-
-            reltimes = [1.0/((t+1e-10)/abstimes[2]) for t in abstimes]
-            absline = absline + "".join(["{:10.5}".format(t) for t in abstimes])
-            relline = relline + "".join(["{:10.5}".format(t) for t in reltimes])
-
-            if relorabs == 'abs':
-                print absline
-            elif relorabs == 'rel':
-                print relline
-        print "\n\n"
-
-    def getTestsWithBackend(self, backend):
-        l = [t for t in self._tests if t.getBackend() == backend]
-        return l
-        
-    def getTestsWithNumthreads(self, num): # only from 'backend_cpu_mt'
-        l = [t for t in self._tests if t.getNumthreads() == num and t.getBackend() == 'backend_cpu_mt']
-        return l
-
-    def getTestWithBackendAndNumthreads(self, backend, num):
-        for t in self._tests:
-            if t.getBackend() == backend and t.getNumthreads() == int(num):
-                return t
-
-    def runAll(self):
-        for test in self._tests:
-            if (test.getBackend() is not "backend_cpu_mt") and (test.getNumthreads() is not 1):
-                pass
-            else:
-                test.printInfoStart()       
-                test.run()
-                test.printInfoEnd()       
-
-        self.printScalingInfo('abs')
-        self.printScalingInfo('rel')
+        pass
+        # match = makeKernelRegex(kernelName).search(self._output)
+        # self._timingValue[kernelName] = float(match.group(2))
+        # self._timingUnit[kernelName] = match.group(3)
 
---
+++ examples/test_final_cost.py
@@ -0,0 +1,172 @@
+from Test import ExampleRun
+import re
+
+# Runs all examples to see if their costs are correct. For this purpose, we
+# use very small problem sizes and a very small number of iterations.
+# USAGE:
+# > python test_final_cost.py
+
+# -----------------------------------------------------------------------------
+# define list of folders to run:
+folders = []
+
+# The problem size here is a few hundred unknowns. We cannot go smaller because
+# that causes some segfault in the cpp code. May we can fix this at a later point.
+# folders.append("arap_mesh_deformation")
+
+# folders.append("cotangent_mesh_smoothing")
+
+# folders.append("embedded_mesh_deformation")
+
+# folders.append("image_warping")
+
+# # the error here can be around 1e-5 with stride=1, so we use stride=12
+# folders.append("intrinsic_image_decomposition")
+
+folders.append("optical_flow")
+
+# the error here will be slightly too large (~1.1e-6) for 4 threads,
+# so we use stride = 4
+# folders.append("poisson_image_editing")
+
+# # this is broken at the moment so ignore the error, need to find out when this happened
+# # Run anyway to make sure we don't get compile-time or run-time errors
+# folders.append("robust_nonrigid_alignment") # note: this solver does more than one solve because singleSolve is overwritten by CombinedSolver
+
+# folders.append("shape_from_shading")
+
+# folders.append("volumetric_mesh_deformation")
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# List of reference costs together with
+# reference costs for small problem size and (nIter, nNonlinIter, nLinIter) = (1,1,1)
+# reference cost should always be taken from CUDA!!!
+referenceCosts = {}
+referenceCosts['arap_mesh_deformation'] = 5867.8574 # CUDA
+referenceCosts['cotangent_mesh_smoothing'] = 10534.742 # CUDA
+referenceCosts['embedded_mesh_deformation'] = 0.36712926 # CUDA
+referenceCosts['image_warping'] = 1774.3405 # CUDA
+referenceCosts['intrinsic_image_decomposition'] = 3.3105300000e6 #CUDA, stride=12 (53x30 px)
+referenceCosts['poisson_image_editing'] = 1530364.25 # CUDA, stride=4 (112x80 px)
+referenceCosts['optical_flow'] = -1
+referenceCosts['robust_nonrigid_alignment'] = 66.784683 # CUDA cost of first!!! solve
+referenceCosts['shape_from_shading'] = -1
+referenceCosts['volumetric_mesh_deformation'] = 189.74081 # CUDA
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# list of strides, if -1, then program will use default stride, i.e. 1 for
+# pixelstuff and 0 subdivisions for graph stuff
+strides = {}
+strides['arap_mesh_deformation'] = -1
+strides['cotangent_mesh_smoothing'] = -1
+strides['embedded_mesh_deformation'] = -1
+strides['image_warping'] = -1
+strides['intrinsic_image_decomposition'] = 12
+strides['optical_flow'] = -1
+strides['poisson_image_editing'] = 4
+strides['robust_nonrigid_alignment'] = -1
+strides['shape_from_shading'] = -1
+strides['volumetric_mesh_deformation'] = -1
+# -----------------------------------------------------------------------------
+
+backends = ["backend_cuda", "backend_cpu", "backend_cpu_mt"]
+# backends = ["backend_cpu", "backend_cpu_mt"]
+numthreads = ["1", "2", "4", "8"]
+# numthreads = ["1", "2", "4", "8", "16", "32"]
+# numthreads = ["4", "8"]
+
+
+#------------------------------------------------------------------------------
+# FUNCTION TO PARSE OUTPUT
+def getFinalCostFromRawOutput(output):
+    match = re.search("final cost=(.*)", output)
+    finalCost = float(match.group(1))
+    return finalCost
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+# TEST CLASS
+class Test:
+    def __init__(self):
+        self._expected = 0.0
+        self._actual = -1.0
+        self._isOk = False
+        self._infoString = ""
+        self._hasFinished = False
+        self._error = -1
+
+    def compare(self):
+        self._hasFinished = True
+        self._error = abs(self._expected - self._actual)/self._expected
+        # all examples should pass with tol=1e-6 except if mentioned otherwise
+        # in the comments at the top.
+        self._isOk = abs(self._error) < 1e-6
+
+    def printInfo(self):
+        if self._hasFinished:
+            if self._isOk:
+                print("{}OK{}, error is ".format('\033[92m','\033[0m') + str(self._error))
+            else:
+                print("{}NOT OK: {}".format('\033[91m','\033[0m') + self._infoString)
+                print("        error is " + str(self._error))
+        else:
+            print("UNDEFINED")
+
+
+#------------------------------------------------------------------------------
+
+
+
+#------------------------------------------------------------------------------
+# RUN EVERYTHING
+tests = []
+for homedir in folders:
+    print ""
+    print 'begin ' + homedir
+    for backend in backends:
+        for num in numthreads:
+            # only do simulation if combination of parameters makes sense.
+            if (backend == "backend_cuda" or backend == "backend_cpu") and int(num) > 1:
+                break
+
+            #-----------------------------------------------
+            # create, define and run the example
+            t = ExampleRun(homedir)
+            t._printOutput = False
+            t._execCommand = "./" + homedir
+            t._args = ["--backend", backend,
+                "--numthreads", num]
+
+            # append stride flag if necessary
+            stride = strides[homedir]
+            if stride > 0:
+                t._args += ['--stride', str(stride)]
+
+
+            t.run()
+            #-----------------------------------------------
+
+
+            # get final cost from raw text output
+            finalCost = float(getFinalCostFromRawOutput(t._output))
+            print(finalCost)
+
+            # compare against reference cost and throw error if necessary
+            test = Test()
+            test._expected = referenceCosts[homedir]
+            test._actual = finalCost
+            test._infoString = str(t.getCallCommand())
+            test.compare()
+            tests.append(test)
+
+    print 'end ' + homedir
+    print ""
+
+
+# print summary
+for t in tests:
+    t.printInfo()
+
+
+
